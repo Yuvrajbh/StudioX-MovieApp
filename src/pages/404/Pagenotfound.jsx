dbdd532043a89559260da1229d1c0bc3924c9014
Yuvrajbh <yuvrajbhalla05@gmail.com>
2024-04-15 01:36:22 +0530

    404 page

--- src/pages/404/Pagenotfound.jsx
+++ src/pages/404/Pagenotfound.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
 
-const Pagenotfound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import "./style.scss";
 
-export default Pagenotfound
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
-
+export default PageNotFound;
